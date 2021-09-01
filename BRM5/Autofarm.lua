2224704ab2229128885f2237e41aed9f6925c617
AlexR32 <58864658+AlexR32@users.noreply.github.com>
2021-09-01 19:30:10 +0300

    menu check and  code cleanup

--- BRM5/Autofarm.lua
+++ BRM5/Autofarm.lua
@@ -1,8 +1,9 @@
 local Network = loadstring(game:HttpGet("https://raw.githubusercontent.com/AlexR32/Roblox/main/BRM5/Network.lua"))() -- by Averias https://v3rmillion.net/showthread.php?tid=1096231 <3
+local Notification = loadstring(game:HttpGet("https://api.irisapp.ca/Scripts/IrisBetterNotifications.lua"))() -- thanks to iris for better notifications
+
 local UserInputService = game:GetService("UserInputService")
 local RunService = game:GetService("RunService")
 local GuiService = game:GetService("GuiService")
-local StarterGui = game:GetService("StarterGui")
 
 local PlayerService = game:GetService("Players")
 local LocalPlayer = PlayerService.LocalPlayer
@@ -25,7 +26,7 @@ local iswindowactive = iswindowactive or isrbxactive
 
 -- nice parachute bypass
 local Float = Instance.new("Part")
-Float.Name = "Autofarm AntiParachute (DONT TOUCH)"
+Float.Name = "autofarm antiparachute (DONT TOUCH)"
 Float.Parent = Workspace
 Float.Transparency = 1
 Float.Anchored = true
@@ -33,15 +34,51 @@ Float.Size = Vector3.new(10,1,10)
 
 getgenv().AutofarmConfig = {
     Enabled = false,
-    BagsFarm = false, -- work in progress
-    Align = CFrame.new(0,0,0) -- aling your character to npc
+    BagsFarm = false, -- work in progress (DONT USE IT for now)
+    Align = CFrame.new(0,0,0) -- aling your character to npc (pretty useless but i let it be just in case)
 }
 
-local function alert(message)
-    StarterGui:SetCore("SendNotification",{
-        Title = "Autofarm\nby AlexR32#3232\n", -- ye made by AlexR32
-        Text = message,
-        Duration = 1
+local function alert(Message, Duration)
+    Notification.Notify("autofarm by AlexR32#3232", Message, "", {
+        Duration = Duration,
+        TitleSettings = {
+            BackgroundColor3 = Color3.fromRGB(255, 255 ,255),
+            TextColor3 = Color3.fromRGB(255, 255, 255),
+            TextScaled = false,
+            TextWrapped = false,
+            TextSize = 14,
+            Font = Enum.Font.Code,
+            TextXAlignment = Enum.TextXAlignment.Left,
+            TextYAlignment = Enum.TextYAlignment.Bottom
+        },
+        DescriptionSettings = {
+            BackgroundColor3 = Color3.fromRGB(255, 255 ,255),
+            TextColor3 = Color3.fromRGB(255, 255, 255),
+            TextScaled = true,
+            TextWrapped = true,
+            TextSize = 18,
+            Font = Enum.Font.Code,
+            TextXAlignment = Enum.TextXAlignment.Left,
+            TextYAlignment = Enum.TextYAlignment.Top,
+        },
+        IconSettings = {
+            BackgroundTransparency = 1,
+            BackgroundColor3 = Color3.fromRGB(255,255,255),
+        },
+        GradientSettings = {
+            GradientEnabled = false,
+            SolidColorEnabled = true,
+            SolidColor = Color3.fromRGB(120,120,120),
+            Retract = false,
+            Extend = true,
+        },
+        Main = {
+            BorderColor3 = Color3.fromRGB(0,0,0),
+            BackgroundColor3 = Color3.fromRGB(30,30,30),
+            BackgroundTransparency = 0.5,
+            Rounding = false,
+            BorderSizePixel = 1
+        }
     })
 end
 
@@ -58,6 +95,14 @@ local function isAlive()
     end
 end
 
+local function checkMenu() -- basic menu check and deploy button fire
+    local Menu = PlayerGui.Screen["#menu"]
+    if Menu.Visible then
+        local Deploy = Menu["#screens"].template["#tab"]["#deploy"]
+        getconnections(Deploy.MouseButton1Click)[1]:Fire()
+    end
+end
+
 local function checkAmmo() -- simple check ammo based on player's gui
     local Clip = PlayerGui.Screen["#main"]["#hud"].right["#ammo"]["#clip"]
     if Clip.Text == "0" then
@@ -71,7 +116,7 @@ local function getAmmo() -- hacky method how to get ammo fast
     for _, Prop in pairs(PropFolder:GetDescendants()) do
         if Prop:IsA("StringValue") then
             if Prop.Value == '[["refillAmmo","Refill Ammo",[]]]' then -- find ammo box
-                if LocalPlayer.Character and LocalPlayer.Character:FindFirstChild("HumanoidRootPart") then -- check if player model in workspace
+                if LocalPlayer.Character and LocalPlayer.Character:FindFirstChild("HumanoidRootPart") then -- check for player character
                     Prop.Parent.CFrame = CFrame.new(-3500, 50, 975) -- tp ammo box under grass
                     LocalPlayer.Character.HumanoidRootPart.CFrame = Prop.Parent.CFrame * CFrame.new(0,2,0) -- tp player on ammo box
                 end
@@ -156,7 +201,7 @@ end
 local function returnHit(target, args)
     CameraPosition = Camera.CFrame.Position
     if args[1].Origin == CameraPosition then
-        args[1] = Ray.new(CameraPosition, (target.Position - CameraPosition))
+        args[1] = Ray.new(CameraPosition, target.Position - CameraPosition)
         return
     end
 end
@@ -176,34 +221,33 @@ end)
 RunService.RenderStepped:Connect(function()
     if AutofarmConfig.Enabled and not MenuOpened and not BagDelivering then
         if iswindowactive() then
-            --if isAlive() then -- replaced to heartbeat event
-                if checkGun(false) then
-                    if checkAmmo() then
-                        target = getTarget()
-                        if target then
-                            mouse1click()
-                        else
-                            if LocalPlayer.Character and LocalPlayer.Character:FindFirstChild("HumanoidRootPart") then
-                                LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-1705, 800, -4532)
-                                Float.Position = LocalPlayer.Character.HumanoidRootPart.Position - Vector3.new(0,4,0)
-                            end
-                        end
+            if checkGun(false) then
+                if checkAmmo() then
+                    target = getTarget()
+                    if target then
+                        mouse1click()
                     else
-                        getAmmo()
+                        if LocalPlayer.Character and LocalPlayer.Character:FindFirstChild("HumanoidRootPart") then
+                            LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-1705, 800, -4532)
+                            Float.Position = LocalPlayer.Character.HumanoidRootPart.Position - Vector3.new(0,4,0)
+                        end
                     end
+                else
+                    getAmmo()
                 end
-            --end
+            end
         end
     end
 end)
 
--- delay for some functions
+-- delay for some functions (dont ask me why i am not using while true do loop or any other shit)
 local Wait = 0
 local WaitMax = 2
 RunService.Heartbeat:Connect(function(Delta)
     Wait += Delta
     if Wait >= WaitMax then
         if AutofarmConfig.Enabled then
+            checkMenu()
             checkGun(true)
             isAlive()
         end
@@ -216,11 +260,12 @@ RunService.Heartbeat:Connect(function(Delta)
     end
 end)
 
--- simple keybind
+-- simple keybind with alert
 UserInputService.InputBegan:Connect(function(Input)
     if Input.KeyCode == Enum.KeyCode.F6 then
         AutofarmConfig.Enabled = not AutofarmConfig.Enabled
-        alert("Autofarm set to " .. tostring(AutofarmConfig.Enabled))
+
+        alert("autofarm " .. (AutofarmConfig.Enabled and "enabled" or "disabled"),1)
     end
 end)
 
@@ -233,4 +278,4 @@ GuiService.MenuClosed:Connect(function()
     MenuOpened = false
 end)
 
-alert("Welcome" .. LocalPlayer.Name) -- send message when script is executed
+alert("press F6 to start farming",5) -- send message when script is executed
